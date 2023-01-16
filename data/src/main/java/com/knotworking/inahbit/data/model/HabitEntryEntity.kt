647b6555476470dc7e81808f41d84f5af92dceda
Ben Russell <ben.russell@approppo.ch>
2023-01-16 13:55:07 +0100

    Add foreign key constraint for HabitEntry -> Habit

--- data/src/main/java/com/knotworking/inahbit/data/model/HabitEntryEntity.kt
+++ data/src/main/java/com/knotworking/inahbit/data/model/HabitEntryEntity.kt
@@ -2,10 +2,19 @@ package com.knotworking.inahbit.data.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.util.*
 
-@Entity(tableName = "habit_entry")
+@Entity(
+    tableName = "habit_entry",
+    foreignKeys = [ForeignKey(
+        entity = HabitEntity::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("habit_id"),
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class HabitEntryEntity(
     @PrimaryKey val id: UUID,
     @ColumnInfo(name = "habit_id") val habitId: UUID,
