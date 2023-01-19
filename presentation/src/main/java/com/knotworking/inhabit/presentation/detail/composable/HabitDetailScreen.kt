9b818bf8c734e3ac6576c64ae5349d340095f466
Ben Russell <ben.russell@approppo.ch>
2023-01-19 13:45:34 +0100

    display habit entries on detail screen

--- presentation/src/main/java/com/knotworking/inhabit/presentation/detail/composable/HabitDetailScreen.kt
+++ presentation/src/main/java/com/knotworking/inhabit/presentation/detail/composable/HabitDetailScreen.kt
@@ -1,7 +1,9 @@
 package com.knotworking.inhabit.presentation.detail.composable
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,6 +17,8 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.knotworking.inhabit.presentation.common.LoadingContent
 import com.knotworking.inhabit.presentation.detail.HabitDetailViewModel
 import com.knotworking.inhabit.presentation.common.model.HabitDisplayable
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
 fun HabitDetailScreen(
@@ -54,5 +58,39 @@ fun HabitDetailContent(
             fontSize = 30.sp
         )
         Text(text = "${habit.entries.size} entries")
+        Spacer(modifier = Modifier.height(20.dp))
+        LazyColumn(
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(habit.entries) { entry ->
+                HabitEntryRow(entry = entry)
+            }
+        }
     }
+}
+
+@Composable
+fun HabitEntryRow(entry: HabitDisplayable.Entry) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = entry.id.toString().take(5))
+            Text(text = entry.timestamp.toDateTime())
+        }
+    }
+}
+
+//TODO needs refactoring, should be somewhere else, probably don't have to do this for each row
+fun Long.toDateTime(): String {
+    val sdf = SimpleDateFormat("dd/MM/yy hh:mm a")
+    val netDate = Date(this)
+    return sdf.format(netDate)
 }
